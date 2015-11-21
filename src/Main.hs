e0185a52aac26f9ab33c6a6f6a2ec6392d5da558
ga2arch <gabriele.carrettoni@gmail.com>
2015-11-21 01:23:42 +0100

    added sql backend

--- src/Main.hs
+++ src/Main.hs
@@ -1,40 +1,73 @@
-{-# LANGUAGE LambdaCase        #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
 module Main where
 
 import           Control.Concurrent
-import Control.Concurrent.Async
+import           Control.Concurrent.Async
 import           Control.Concurrent.MVar
 import           Control.Monad
-import           Data.ByteString         (ByteString)
-import           Data.Hashable           (Hashable)
+import           Control.Monad.IO.Class
+import           Control.Monad.Logger
+import           Control.Monad.Trans
+import           Control.Monad.Trans.Resource
+import           Data.ByteString              (ByteString)
+import           Data.Hashable                (Hashable)
 import           Data.Monoid
+import           Data.Pool                    (Pool)
 import           Data.Streaming.Network
+import           Data.Text                    (Text)
 import           Data.Time.Clock.POSIX
+import           Database.Esqueleto
+import           Database.Persist             (selectList)
+import           Database.Persist.Sqlite      (createSqlitePool, runSqlPool)
+import           Database.Persist.TH
 
-import qualified Data.ByteString.Char8   as C
-import qualified Data.HashMap.Strict     as H
+import qualified Data.ByteString.Char8        as C
+import qualified Data.HashMap.Strict          as H
 
 data Result = Done
             | Entry !ByteString !Int
             | Error !ByteString
     deriving (Show)
 
-forkOnNew ad value time = forkIO $ do
-    threadDelay $ time * 10^6
-    appWrite ad $ value <> "\n"
+share [mkPersist sqlSettings, mkMigrate "migrateTables"] [persistLowerCase|
+TimeEntry
+   value     ByteString
+   timestamp Int
 
-mapBased mh ad value time = do
+   deriving Show
+|]
+
+insertEntry mh next value = modifyMVar_ mh $ \h -> do
+    let bucket = H.lookupDefault [] next h
+    return $ H.insert next (value:bucket) h
+
+strategy :: MVar (H.HashMap Int [ByteString])
+        -> Pool SqlBackend
+        -> t
+        -> ByteString
+        -> Int
+        -> IO ()
+strategy mh pool ad value time = do
     current <- round <$> getPOSIXTime
     let next = current + time
-    modifyMVar_ mh $ \h -> do
-        let bucket = H.lookupDefault [] next h
-        return $ H.insert next (value:bucket) h
+    runDb pool $ insert $ TimeEntry value next
 
-worker :: MVar (H.HashMap Int [ByteString]) -> IO ()
-worker mh = do
-    current <- round <$> getPOSIXTime
-    mapM_ handle $ iterate (+1) current
+    insertEntry mh next value
+
+worker :: MVar (H.HashMap Int [ByteString])
+        -> Pool SqlBackend
+        -> IO ()
+worker mh pool = do
+    now <- round <$> getPOSIXTime
+    mapM_ handle $ iterate (+1) now
 
   where
     handle current = do
@@ -46,7 +79,13 @@ worker mh = do
         threadDelay $ 1 * 10^6
 
     f h current bucket = do
-        async $ mapM_ (\x -> print $ show x <> "timed out") bucket
+        async $ do
+            mapM_ (\x ->
+                print $ show x <> "timed out") bucket
+
+            runDb pool $ delete $ from $ \t ->
+                where_ (t ^. TimeEntryTimestamp ==. val current)
+
         return $ H.delete current h
 
 tcpServer action = runTCPServer (serverSettingsTCP 8080 "*") app
@@ -78,8 +117,23 @@ tcpServer action = runTCPServer (serverSettingsTCP 8080 "*") app
             Just (t, _) -> Entry value t
             Nothing     -> Error "Invalid"
 
+runDb conn f = runResourceT $ runNoLoggingT $ runSqlPool f conn
+
 main :: IO ()
 main = do
     h <- newMVar H.empty
-    async $ worker h
-    tcpServer $ mapBased h
+
+    pool <- runNoLoggingT $ createSqlitePool "timecache.sql" 5
+    runDb pool $ runMigration migrateTables
+    entries <- runDb pool $ select $ from $ \t -> return t
+
+    mapM_ (\x -> do
+        now <- round <$> getPOSIXTime
+
+        let time  = timeEntryTimestamp $ entityVal x
+            value = timeEntryValue     $ entityVal x
+
+        when (time >= now) $ insertEntry h time value) entries
+
+    async $ worker h pool
+    tcpServer $ strategy h pool
